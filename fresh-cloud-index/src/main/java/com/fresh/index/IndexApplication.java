2795c2cf5816d60384dcc4fd87133e2e489e7ad0
McALongYeah <2248365616@qq.com>
2023-12-22 19:26:01 +0800

    新增fresh-cloud-index模块 可以在nacos上注册服务

---
+++ fresh-cloud-index/src/main/java/com/fresh/index/IndexApplication.java
@@ -0,0 +1,17 @@
+package com.fresh.index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+
+// redis 实现 session 共享 和 设置会话时间
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 60 * 8)
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.fresh"})
+public class IndexApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IndexApplication.class,args);
+    }
+}
